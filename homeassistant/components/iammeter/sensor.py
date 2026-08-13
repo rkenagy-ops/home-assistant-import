7208100e8c39c28e52b865feda726591c62c16ce
Sajjad-shayesteh <sajjad.shayesteh@outlook.com>
2026-08-13 11:45:36 +1000

    Fix indentation in DataUpdateCoordinator initialization

--- homeassistant/components/iammeter/sensor.py
+++ homeassistant/components/iammeter/sensor.py
@@ -132,10 +132,10 @@ async def async_setup_platform(
             raise UpdateFailed from err
 
     coordinator = DataUpdateCoordinator(
-    hass,
-    _LOGGER,
-    config_entry=None,
-    name=config_name,
+        hass,
+        _LOGGER,
+        config_entry=None,
+        name=config_name,
         update_method=async_update_data,
         update_interval=SCAN_INTERVAL,
         request_refresh_debouncer=debounce.Debouncer(
